ef2e00a1d3759cb1afd40632d554c5a4029724ea
Rohit Rana <rkrana6631@gmail.com>
2023-12-15 19:38:07 +0530

    check

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user");
 const accountSid = 'AC608c293f7cd37e13823f6a4256a9d25f';
-const authToken = 'ddbd5c157d5f8e27c4de68ed0771ac2e';
+const authToken = '08617c75beb2fdee4448a400b942222c';
 const twilioPhoneNumber = '+12058289919';
 const jwt = require('jsonwebtoken');
 const twilio = require('twilio');
@@ -10,47 +10,45 @@ const secret_key = "Rana";
 
 //REGISTER
 router.post("/register", async (req, res) => {
-    // try {
-    //     const mobileNumber = req.body.mobileNumber;
+    try {
+        const mobileNumber = req.body.mobileNumber;
 
-    //     // Check if user already exists with the given mobile number
-    //     const existingUser = await User.findOne({ mobile: mobileNumber });
+        // Check if user already exists with the given mobile number
+        const existingUser = await User.findOne({ mobile: mobileNumber });
 
-    //     if (existingUser) {
-    //         // User already exists, you might want to handle this case appropriately
-    //         return res.status(400).json({ success: false, message: 'User with this mobile number already exists.' });
-    //     }
+        if (existingUser) {
+            // User already exists, you might want to handle this case appropriately
+            return res.status(400).json({ success: false, message: 'User with this mobile number already exists.' });
+        }
 
-    //     // User does not exist, generate OTP and create a new user
-    //     const otp = Math.floor(100000 + Math.random() * 900000);
-    //     const newUser = new User({
-    //         name: req.body.name,
-    //         mobile: mobileNumber,
-    //         otp: otp
-    //     });
+        // User does not exist, generate OTP and create a new user
+        const otp = Math.floor(100000 + Math.random() * 900000);
+        const newUser = new User({
+            name: req.body.name,
+            mobile: mobileNumber,
+            otp: otp
+        });
 
-    //     const user = await newUser.save();
+        const user = await newUser.save();
 
-    //     // Use Twilio to send OTP
+        // Use Twilio to send OTP
         await client.messages.create({
             body: `Your OTP is: ${otp}`,
             from: twilioPhoneNumber,
             to: "+91 " + mobileNumber
         })
-    //         .then(message => {
-    //             console.log(`OTP sent successfully! ${otp}: ${message.sid}`);
-    //             res.status(200).json({ success: true, message: 'OTP sent successfully.', data: user });
-    //         })
-    //         .catch(error => {
-    //             console.error('Error sending OTP:', error.message);
-    //             res.status(500).json({ success: false, message: 'Failed to send OTP.' });
-    //         });
-    // } catch (error) {
-    //     console.error('Error creating user or sending OTP:', error.message);
-    //     res.status(500).json({ success: false, message: 'Failed to create user or send OTP.' });
-    // }
-    const otp = Math.floor(100000 + Math.random() * 900000);
-    res.status(200).json({ success: true, message: 'OTP sent successfully.' });
+            .then(message => {
+                console.log(`OTP sent successfully! ${otp}: ${message.sid}`);
+                res.status(200).json({ success: true, message: 'OTP sent successfully.', data: user });
+            })
+            .catch(error => {
+                console.error('Error sending OTP:', error.message);
+                res.status(500).json({ success: false, message: 'Failed to send OTP.' });
+            });
+    } catch (error) {
+        console.error('Error creating user or sending OTP:', error.message);
+        res.status(500).json({ success: false, message: 'Failed to create user or send OTP.' });
+    }
 });
 
 // Verify OTP
@@ -91,7 +89,7 @@ router.post("/verify-otp", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const mobileNumber = req.body.mobileNumber;
-
+        console.log(mobileNumber);
 
         // Find the user with the given mobile number
         const user = await User.findOne({ mobile: mobileNumber });
