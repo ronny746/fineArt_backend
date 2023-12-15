75c2d125c7ebad69cb2c5d08c6f47eb15a6e9174
Rohit Rana <rkrana6631@gmail.com>
2023-12-15 18:11:33 +0530

    dd

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 // const app = express();
 // app.use(express.json());
 
+
 // mongoose.connect('mongodb+srv://rohit:rana@cluster0.btddseq.mongodb.net/fineArt?retryWrites=true&w=majority',
 //     { useUnifiedTopology: true, useNewUrlParser: true },
 // ).then(() => app.listen(3000)
@@ -23,16 +24,19 @@ const PORT = 3000;
 // This middleware will not allow the
 // request to go beyond it
 app.use(function (req, res, next) {
-    console.log("Middleware called")
-    next();
+	console.log("Middleware called")
+	next();
 });
-app.get(authRoute);
 
 // Requests will never reach this route
-// app.get(authRoute, function (req, res) {
-// 	console.log("/user request called");
-// 	res.send('Welcome to GeeksforGeeks');
-// });
+app.get('/user',async function (req, res) {
+    const User = require("../models/User");
+	// console.log("/user request called");
+	// res.send('Welcome to GeeksforGeeks');
+    const user = await User.findOne({ mobile: mobileNumber });
+    res.send(user);
+
+});
 
 mongoose.connect('mongodb+srv://rohit:rana@cluster0.btddseq.mongodb.net/fineArt?retryWrites=true&w=majority',
     { useUnifiedTopology: true, useNewUrlParser: true },
