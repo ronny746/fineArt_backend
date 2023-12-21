dc92eca0cd64ab5f22282649110ff311d31993fd
Rohit Rana <rkrana6631@gmail.com>
2023-12-21 11:01:39 +0530

    pk

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const corsOptions = {
-    origin: 'http://localhost:57042', // replace with your Flutter web app domain
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-};
+
 mongoose.connect('mongodb+srv://rohit:rana@cluster0.btddseq.mongodb.net/fineArt?retryWrites=true&w=majority',
     { useUnifiedTopology: true, useNewUrlParser: true },
 ).then(() => app.listen(3000)
@@ -23,7 +18,7 @@ const categoryRoute = require("./routes/category");
 const subcategoryRoute = require("./routes/sub_category");
 const productRoute = require("./routes/product");
 
-app.use("/api/auth", cors(corsOptions), authRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/category", categoryRoute);
 app.use("/api/subcategory", subcategoryRoute);
 app.use("/api/product", productRoute);
