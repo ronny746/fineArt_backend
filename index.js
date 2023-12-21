2d8bdcb093b1959753290c9fee91c79faff2f56e
Rohit Rana <rkrana6631@gmail.com>
2023-12-21 10:52:11 +0530

    pk

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const cors = require('cors');
 const express = require('express');
 const axios = require('axios');
 const mongoose = require("mongoose");
-app.use(cors());
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://rohit:rana@cluster0.btddseq.mongodb.net/fineArt?retryWrites=true&w=majority',
